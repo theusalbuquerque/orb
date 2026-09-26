7442b65d5e3f5d842a69388f52437266624e1e9b
THEUS <81582824+theusalbuquerque@users.noreply.github.com>
2026-09-26 13:08:28 -0300

    Fix history sheet imports

--- app/src/main/java/com/music/orb/ui/screens/PlaybackHistorySheet.kt
+++ app/src/main/java/com/music/orb/ui/screens/PlaybackHistorySheet.kt
@@ -28,7 +28,6 @@ import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.background
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
